09b547c3d41015b70d6994b1eaf31fecb95a6bfe
emieliason <eeliason22@amherst.edu>
2023-02-25 22:21:12 -0500

    misc changes

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
 function getWeather() {
-    let temperature = document.getElementById("temperature");
-    let description = document.getElementById("description");
-    let location = document.getElementById("location");
-  
-    let api = "https://api.openweathermap.org/data/2.5/weather";
-    let apiKey = "f34b50cb362f0c89e2d43ca3b46e50cf";
-  
-    location.innerHTML = "Locating...";
-  
-    navigator.geolocation.getCurrentPosition(success, error);
-  
-    function success(position) {
-      latitude = position.coords.latitude;
-      longitude = position.coords.longitude;
-  
-      let url =
-        api +
-        "?lat=" +
-        latitude +
-        "&lon=" +
-        longitude +
-        "&appid=" +
-        apiKey +
-        "&units=imperial";
-  
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
-          let temp = data.main.temp;
-          temperature.innerHTML = temp + "° F";
-          location.innerHTML =
-            data.name + " (" + latitude + "°, " + longitude + "°)";
-          description.innerHTML = data.weather[0].main;
-        });
-    }
-  
-    function error() {
-      location.innerHTML = "Unable to retrieve your location";
-    }
+  let temperature = document.getElementById("temperature");
+  let description = document.getElementById("description");
+  let location = document.getElementById("location");
+
+  let api = "https://api.openweathermap.org/data/2.5/weather";
+  let apiKey = "f34b50cb362f0c89e2d43ca3b46e50cf";
+
+  location.innerHTML = "Locating...";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    let url =
+      api +
+      "?lat=" +
+      latitude +
+      "&lon=" +
+      longitude +
+      "&appid=" +
+      apiKey +
+      "&units=imperial";
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        let temp = data.main.temp;
+        temperature.innerHTML = temp + "° F";
+        location.innerHTML =
+          data.name + " (" + latitude + "°, " + longitude + "°)";
+        description.innerHTML = data.weather[0].main;
+      });
   }
-  
-  getWeather();
+
+  function error() {
+    location.innerHTML = "Unable to retrieve your location";
+  }
+}
+
+getWeather();
